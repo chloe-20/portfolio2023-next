32ba675f29d6609be582b38e9d355f7537335812
chloe iy choi <ioiuioiciy88@gmail.com>
2023-09-12 16:01:52 +0900

    마무리 완료

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 import { portText } from "@/constants";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const Portfolio = () => {
+  const horizontalRef = useRef(null);
+  const sectionsRef = useRef([]);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const horizontal = horizontalRef.current;
+    const sections = sectionsRef.current;
+
+    let scrollTween = gsap.to(sections, {
+      xPercent: -120 * (sections.length - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: horizontal,
+        start: "top 56px",
+        end: () => "+=" + horizontal.offsetWidth,
+        pin: true,
+        scrub: 1,
+        invalidateOnRefresh: true,
+        anticipatePin: 1,
+      },
+    });
+
+    return () => {
+      scrollTween.kill();
+    };
+  }, []);
+
   return (
-    <section id="portfolio">
+    <section id="portfolio" ref={horizontalRef}>
       <div className="portfolio__inner">
         <div className="portfolio__title">PORTFOLIO <em>포트폴리오 작업물(수정예정)</em></div>
         <div className="portfolio__wrap">
           {portText.map((port, key) => (
-            <article className={`portfolio__item p${key+1}`} key={key}>
+            <article 
+              className={`portfolio__item p${key + 1}`} 
+              key={key} 
+              ref={(el) => (sectionsRef.current[key] = el)}
+            >
               <span className="num">{port.num}.</span>
               <a href={port.code} target="_blank" className="img">
                 <Image src={port.img} alt={port.name} width={420} height={262} />
